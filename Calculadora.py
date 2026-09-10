57344f378a7200faadb3f63037f0e0c3fea2a7e9
Manuel Mosquera <ingemanu0@gmail.com>
2026-09-09 19:02:04 -0500

    Funcionamineto exitoso

--- Calculadora.py
+++ Calculadora.py
@@ -18,39 +18,65 @@ def calcular():
             r = a % b
         elif op == "**": r = a ** b
         else: raise ValueError("Operación no válida")
-        
+
         label_resultado.config(text=f"Resultado: {r}")
     except ValueError:
-        messagebox.showerror("Error", "Ingrese números válidos")
+        messagebox.showerror("Error", "Ingrese números válidos y seleccione una operación")
     except ZeroDivisionError:
         messagebox.showerror("Error", "No se puede dividir entre cero")
 
+    chk_var.set(False)
+
+
+def usar_anterior():
+    if chk_var.get():
+        texto_resultado = label_resultado.cget("text")
+        partes = texto_resultado.split(": ")
+        
+        # Verificar si existe un resultado numérico calculado
+        if len(partes) > 1 and partes[1].strip() != "":
+            entry_a.delete(0, tk.END)
+            entry_a.insert(0, partes[1])
+        else:
+            messagebox.showinfo("Información", "Aún no hay un resultado previo para cargar")
+            chk_var.set(False)
+    else:
+        entry_a.delete(0, tk.END)
+        entry_b.delete(0, tk.END)
+        label_resultado.config(text="Resultado: ")
 
 
 ventana = tk.Tk()
 ventana.title("Calculadora")
 ventana.geometry("500x400")
+ventana.configure(bg="lightblue")
 
 
 
-tk.Label(ventana, text="Primer número:").pack()
+tk.Label(ventana, text="Primer número:", bg="lightblue").pack()
 entry_a = tk.Entry(ventana)
 entry_a.pack()
 
-tk.Label(ventana, text="Segundo número:").pack()
+
+tk.Label(ventana, text="Segundo número:", bg="lightblue").pack()
 entry_b = tk.Entry(ventana)
 entry_b.pack()
 
-tk.Label(ventana, text="Presione para desplegar: ").pack()
-operacion = tk.StringVar(value="+")
+
+tk.Label(ventana, text="Presione para desplegar: ", bg="lightblue").pack()
+operacion = tk.StringVar(value="Operaciones")
 tk.OptionMenu(ventana, operacion, "+", "-", "*", "/", "%", "**").pack()
 
 
-tk.Button(ventana, text="Calcular", command=calcular).pack(pady=20)
+
+chk_var = tk.BooleanVar()
+tk.Checkbutton(ventana, text="Usar anterior", variable=chk_var, command=usar_anterior).pack()
 
 
+tk.Button(ventana, text="Calcular", command=calcular, bg="lightblue").pack(pady=20)
 
-label_resultado = tk.Label(ventana, text="Resultado: ", font=("Arial", 20, "bold"))
+
+label_resultado = tk.Label(ventana, text="Resultado: ", font=("Arial", 20, "bold"), bg="lightgreen")
 label_resultado.pack(pady=10)
 
 
